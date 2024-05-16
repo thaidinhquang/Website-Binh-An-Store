4ac2a8dc52e698184b54c5b6fa902061fd53119d
lvcuong25 <vietcuong2532k3@gmail.com>
2024-05-16 21:52:39 +0700

    First Commit

---
+++ BE/src/app.js
@@ -0,0 +1,21 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+import { connectDB } from "./config/db";
+
+const app = express();
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// connect db
+connectDB(process.env.DB_URI);
+
+// routers
+app.use("/", consolelog("sv running on port:8080..."));
+
+
+export const viteNodeApp = app;
---
+++ FE/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primarygray: "#f8f8f8",
+        qblack: "#222222",
+        qyellow: "#FFBB38",
+        qred: "#EF262C",
+        qgray: "#797979",
+        qblacktext: "#1D1D1D",
+        qgraytwo: "#8E8E8E",
+        "qgray-border": "#EFEFEF",
+        "qblue-white": "#CBECFF",
+        "qh2-green": "#2D6F6D",
+        "qh4-pink": "#FDB2BB",
+        "qh5-bwhite": "#95D7DE",
+        "qh3-blue": "#1868D5",
+      },
+      scale: {
+        60: "0.6",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ["focus-within"],
+      borderStyle: ["last"],
+    },
+  },
+};
