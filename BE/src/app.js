99e90f8fe1cf8d7e5d8fd4d97343603be2c3b54a
MaiHuonggg <huongltmph28400@fpt.edu.vn>
2024-06-02 16:03:10 +0700

    BE category

--- BE/src/app.js
+++ BE/src/app.js
@@ -1,6 +1,9 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+import categoryRouter from "./routes/category";
+
 
 import { connectDB } from "./config/db";
 
@@ -10,6 +13,7 @@ dotenv.config();
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use("/api", categoryRouter)
 
 // connect db
 connectDB(process.env.DB_URI);
@@ -17,5 +21,24 @@ connectDB(process.env.DB_URI);
 // routers
 app.use("/", consolelog("sv running on port:8080..."));
 
+mongoose.connect("mongodb://127.0.0.1:27017/DATN", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Sự kiện khi kết nối thành công
+mongoose.connection.on("connected", () => {
+    console.log("Kết nối đến MongoDB thành công");
+});
+
+// Sự kiện khi kết nối bị lỗi
+mongoose.connection.on("error", (err) => {
+    console.error("Kết nối đến MongoDB thất bại:", err);
+});
+
+// Sự kiện khi ngắt kết nối
+mongoose.connection.on("disconnected", () => {
+    console.log("Ngắt kết nối đến MongoDB");
+});
 
 export const viteNodeApp = app;
