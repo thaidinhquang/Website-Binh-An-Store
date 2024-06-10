706152bf86a3d999251a6a691fd054a8979aee9d
thaidinhquang <144780282+thaidinhquang@users.noreply.github.com>
2024-06-10 11:41:03 +0700

    fake API Admin-Users

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -16,6 +16,8 @@ import CategoryAdd from "./components/Admin/Category/CategoryAdd";
 import CategorytEdit from "./components/Admin/Category/CategoryEdit";
 import CartPage from "./components/CartPage/CartPage";
 import Signup from "./components/Auth/SignUp";
+import UserList from "./components/Admin/Users/UserList";
+import UserEdit from "./components/Admin/Users/UserEdit";
 
 function App() {
   return (
@@ -28,27 +30,33 @@ function App() {
         expand={true}
       />
       <Routes>
-        <Route path="/" element={<LayOutHome/>}>
+        <Route path="/" element={<LayOutHome />}>
           <Route index element={<Home />} />
-          <Route path="detail/:id" element={<SingleProduct/>}/>
+          <Route path="detail/:id" element={<SingleProduct />} />
           <Route path="about" element={<About />} />
           <Route path="cart" element={<CartPage />} />
           <Route path="checkout" element={<CheckoutPage />} />
-          <Route path="login" element={<Login/>}/>
+          <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
         </Route>
 
-        <Route path="/admin" element={<LayOutAdmin/>}>
+        <Route path="/admin" element={<LayOutAdmin />}>
           <Route path="product">
             <Route index element={<ProductList />} />
             <Route path="add" element={<ProductAdd />} />
             <Route path="edit/:id" element={<ProductEdit />} />
           </Route>
           <Route path="category">
-          <Route index element={<CategorytList />} />
-          <Route path="add" element={<CategoryAdd />} />
-          <Route path="edit/:id" element={<CategorytEdit />} />
-        </Route>
+            <Route index element={<CategorytList />} />
+            <Route path="add" element={<CategoryAdd />} />
+            <Route path="edit/:id" element={<CategorytEdit />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UserList />} />
+
+            <Route path="edit/:id" element={<UserEdit />} />
+          </Route>
         </Route>
       </Routes>
     </>
