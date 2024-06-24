0567b6526cd7a983e70f28741536cb280da16283
anhastrung <anhastrung@gmail.com>
2024-06-25 00:00:38 +0700

    order

---
+++ BE/src/constants/order.js
@@ -0,0 +1,12 @@
+export const ORDER_STATUS = {
+  PENDING: "PENDING",
+  CONFIRMED: "CONFIRMED",
+  SHIPPING: "SHIPPING",
+  DELIVERED: "DELIVERED",
+  DONE: "DONE",
+};
+
+export const PAYMENT_METHOD = {
+  CASH: "cash",
+  CARD: "card",
+};
--- BE/src/routes/index.js
+++ BE/src/routes/index.js
@@ -5,6 +5,7 @@ import routerCategory from "./categories.js";
 import routerProduct from "./product.js";
 import routerCart from "./cart.js";
 import routerUser from "./user.js";
+import routerOrder from "./order.js";
 
 const router = Router();
 
@@ -14,4 +15,5 @@ router.use("/categories", routerCategory);
 router.use("/products", routerProduct);
 router.use("/cart", routerCart);
 router.use("/users", routerUser);
+router.use("/orders", routerOrder);
 export default router;
