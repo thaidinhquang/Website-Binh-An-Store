59fa0b4fe4d764787e466c7d954f99ec639dccf2
thaidinhquang <144780282+thaidinhquang@users.noreply.github.com>
2024-07-16 18:13:25 +0700

    update DashBoard admin

--- FE/src/components/Admin/DashBoard.jsx
+++ FE/src/components/Admin/DashBoard.jsx
@@ -1,14 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
 import IcoDashboard from "../icons/IcoDashboard";
 import IcoPeople from "../icons/IcoPeople";
-import IcoPayment from "../icons/IcoPayment";
+
 import IcoCart from "../icons/IcoCart";
 import IcoLove from "../icons/IcoLove";
-import IcoAdress from "../icons/IcoAdress";
-import IcoReviewHand from "../icons/IcoReviewHand";
-import IcoPassword from "../icons/IcoPassword";
-import IcoSupport from "../icons/IcoSupport";
-import IcoLogout from "../icons/IcoLogout";
 import Product from "../icons/Product";
 import IcoCategory from "../icons/IcoCategory";
 
@@ -28,7 +23,7 @@ const DashBoard = () => {
                 <div className="w-[236px] min-h-[600px] border-r border-[rgba(0, 0, 0, 0.1)] ">
                   <div className="flex flex-col space-y-10 fixed h-screen overflow-y-auto no-scrollbar">
                     <div className="item group">
-                      <Link to="/profile#dashboard">
+                      <Link to="/admin">
                         <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
                           <span>
                             <IcoDashboard />
@@ -39,20 +34,6 @@ const DashBoard = () => {
                         </div>
                       </Link>
                     </div>
-
-                    <div className="item group">
-                      <Link to="/admin/products">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <Product />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Sản phẩm
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-
                     <div className="item group">
                       <Link to="/admin/categories">
                         <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
@@ -65,7 +46,18 @@ const DashBoard = () => {
                         </div>
                       </Link>
                     </div>
-
+                    <div className="item group">
+                      <Link to="/admin/products">
+                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
+                          <span>
+                            <Product />
+                          </span>
+                          <span className=" font-normal text-base">
+                            Sản phẩm
+                          </span>
+                        </div>
+                      </Link>
+                    </div>
                     <div className="item group">
                       <Link to="/admin/users">
                         <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
@@ -74,19 +66,6 @@ const DashBoard = () => {
                           </span>
                           <span className=" font-normal text-base">
                             Người dùng
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-
-                    <div className="item group">
-                      <Link to="/profile#payment">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoPayment />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Payment Method
                           </span>
                         </div>
                       </Link>
@@ -104,73 +83,13 @@ const DashBoard = () => {
                       </Link>
                     </div>
                     <div className="item group">
-                      <Link to="/profile#wishlist">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
+                      <Link to="/">
+                        <div className="flex space-x-3 items-center text-green-500 hover:text-qblack">
                           <span>
                             <IcoLove />
                           </span>
                           <span className=" font-normal text-base">
-                            Wishlist
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#address">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoAdress />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Address
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#review">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoReviewHand />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Reviews
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#password">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoPassword />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Change Password
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#support">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoSupport />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Support Ticket
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="item group">
-                      <Link to="/profile#profile">
-                        <div className="flex space-x-3 items-center text-qgray hover:text-qblack">
-                          <span>
-                            <IcoLogout />
-                          </span>
-                          <span className=" font-normal text-base">
-                            Logoout
+                            Website
                           </span>
                         </div>
                       </Link>
