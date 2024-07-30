4079d972f500f5c2004f109665fb43823992039c
hungh <hunghsph30697@fpt.edu.vn>
2024-07-30 13:17:55 +0700

    chore: Remove trailing whitespace and fix merge conflicts

--- FE/src/common/hooks/useTanstackQuery.js
+++ FE/src/common/hooks/useTanstackQuery.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { axiosDelete, axiosGet, axiosPatch, axiosPost, axiosPut } from "../../config/axios";
 import { useForm } from "react-hook-form";
