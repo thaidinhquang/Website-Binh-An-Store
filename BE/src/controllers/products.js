b30c77d1ae9768fbb0959af867a42a519f38f7dd
hungh <hunghsph30697@fpt.edu.vn>
2024-08-21 07:22:10 +0700

    chore: Update product schema and routes for details and review

--- BE/src/controllers/products.js
+++ BE/src/controllers/products.js
@@ -2,7 +2,8 @@ import { populate } from "dotenv";
 import Category from "../models/Category.js";
 import Brand from "../models/Brand.js";
 import Product from "../models/Product.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import ProductItem from "../models/ProductItem.js";
 
 export const getAllProduct = async (req, res, next) => {
   try {
@@ -11,38 +12,45 @@ export const getAllProduct = async (req, res, next) => {
       limit: req.query.limit ? +req.query.limit : 10,
       sort: req.query.sort ? req.query.sort : { createdAt: -1 },
       populate: [
-        { path: 'category' },
-        { path: 'brand' },
-        { path: 'attributes', populate: { path: 'values', select: 'name price quantity active' }  }
-      ]
+        { path: "category" },
+        { path: "brand" },
+        {
+          path: "attributes",
+          populate: { path: "values", select: "name price quantity active" },
+        },
+      ],
     };
     let query = {};
     if (req.query.id) {
       query._id = req.query.id;
     }
     if (req.query.name) {
-      query.name = { $regex: new RegExp(req.query.name, 'i') };
+      query.name = { $regex: new RegExp(req.query.name, "i") };
     }
     if (req.query.slug) {
-      query.slug = { $regex: new RegExp(req.query.slug, 'i') };
+      query.slug = { $regex: new RegExp(req.query.slug, "i") };
     }
     if (req.query.categories) {
-      const categoryIds = req.query.categories.split(',');
+      const categoryIds = req.query.categories.split(",");
       query.category = { $in: categoryIds };
-    } 
+    }
     if (req.query.brand) {
-      const brandIds = req.query.brand.split(',');
+      const brandIds = req.query.brand.split(",");
       query.brand = { $in: brandIds };
     }
     if (req.query.attributes) {
-      const attributesIds = req.query.attributes.split(',');
+      const attributesIds = req.query.attributes.split(",");
       query.attributes = { $in: attributesIds };
-    } 
+    }
     if (req.query.active) {
-      query.active = req.query.active === 'true';
+      query.active = req.query.active === "true";
     }
     const data = await Product.paginate(query, options);
-    return !data ? res.status(400).json({ message: "Không tìm thấy sản phẩm nào!" }) : res.status(200).json({ data, message: "Lấy danh sách sản phẩm thành công"});
+    return !data
+      ? res.status(400).json({ message: "Không tìm thấy sản phẩm nào!" })
+      : res
+          .status(200)
+          .json({ data, message: "Lấy danh sách sản phẩm thành công" });
   } catch (error) {
     next(error);
   }
@@ -50,58 +58,89 @@ export const getAllProduct = async (req, res, next) => {
 
 export const getDetailProductPopulate = async (req, res, next) => {
   try {
-    const data = await Product.findById(req.params.id).populate("category").populate("brand")  .populate({
-      path: 'attributes',
-      populate: { path: 'values', select: 'name price quantity active' } 
-    });
-    return !data ? res.status(400).json({ message: "Khong tim thay san pham!" }) : res.status(200).json({ data })
+    const data = await Product.findById(req.params.id)
+      .populate("category")
+      .populate("brand")
+      .populate({
+        path: "attributes",
+        populate: { path: "values", select: "name price quantity active" },
+      });
+    return !data
+      ? res.status(400).json({ message: "Khong tim thay san pham!" })
+      : res.status(200).json({ data });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const getDetailProduct = async (req, res, next) => {
   try {
-    const data = await Product.findById(req.params.id)
-    return !data ? res.status(400).json({ message: "Khong tim thay san pham!" }) : res.status(200).json({ data })
+    const data = await Product.findById(req.params.id);
+    return !data
+      ? res.status(400).json({ message: "Khong tim thay san pham!" })
+      : res.status(200).json({ data });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const deleteProduct = async (req, res, next) => {
   try {
-    const data = await Product.findByIdAndUpdate(req.params.id, { active: false }, { new: true });
-    return !data ? res.status(400).json({ message: "Xoa that bai!" }) : res.status(200).json({ data, message: "Xoa thanh cong!"})
+    const data = await Product.findByIdAndUpdate(
+      req.params.id,
+      { active: false },
+      { new: true }
+    );
+    return !data
+      ? res.status(400).json({ message: "Xoa that bai!" })
+      : res.status(200).json({ data, message: "Xoa thanh cong!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const restoreProduct = async (req, res, next) => {
   try {
-    const data = await Product.findByIdAndUpdate(req.params.id, { active: true }, { new: true });
-    return !data ? res.status(400).json({ message: "Khoi phuc that bai!" }) : res.status(200).json({ data, message: "Khoi phuc thanh cong!"})
+    const data = await Product.findByIdAndUpdate(
+      req.params.id,
+      { active: true },
+      { new: true }
+    );
+    return !data
+      ? res.status(400).json({ message: "Khoi phuc that bai!" })
+      : res.status(200).json({ data, message: "Khoi phuc thanh cong!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-export const createProduct = async (req, res, next) => {
+export const createProduct = async (req, res) => {
   try {
-    const { name } = req.body;
+    if (!req.body.name || !req.body.description || !req.body.category) {
+      throw new Error("Missing required fields");
+    }
+    const product = new Product({ ...req.body });
 
-    // Check tên sản phẩm
-    const existingProduct = await Product.findOne({ name });
-    if (existingProduct) {
-      return res.status(400).json({ message: "Tên sản phẩm đã tồn tại" });
+    await product.save();
+
+    if (product && product._id) {
+      if (req.body.variants && req.body.variants.length) {
+        for (let i = 0; i < req.body.variants.length; i++) {
+          const variant = req.body.variants[i];
+          console.log("Variant:", variant);
+          console.log("Product:", product._id);
+          const productItem = new ProductItem({
+            ...variant,
+            productId: product._id,
+          });
+          await productItem.save();
+        }
+      }
     }
 
-    const data = await Product.create(req.body);
-    await Category.findByIdAndUpdate(data.category, { $push: { products: data._id } });
-    return !data ? res.status(400).json({ message: "Thêm sản phẩm thất bại!" }) : res.status(200).json({ data, message: "Thêm sản phẩm thành công!"})
+    return res.status(200).json({ product });
   } catch (error) {
-    next(error)
+    console.log(error.message);
   }
 };
 
@@ -116,9 +155,15 @@ export const updateProduct = async (req, res, next) => {
       return res.status(400).json({ message: "Tên sản phẩm đã tồn tại" });
     }
 
-    const data = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    return !data ? res.status(400).json({ message: "Cập nhật sản phẩm thất bại!" }) : res.status(200).json({ data, message: "Cập nhật sản phẩm thành công!"})
+    const data = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    return !data
+      ? res.status(400).json({ message: "Cập nhật sản phẩm thất bại!" })
+      : res
+          .status(200)
+          .json({ data, message: "Cập nhật sản phẩm thành công!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
--- BE/src/routes/product.js
+++ BE/src/routes/product.js
@@ -1,18 +1,29 @@
 import { Router } from "express";
-import { createProduct, deleteProduct, getAllProduct, getDetailProduct, getDetailProductPopulate, restoreProduct, updateProduct } from "../controllers/products.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProduct,
+  getDetailProduct,
+  getDetailProductPopulate,
+  restoreProduct,
+  updateProduct,
+} from "../controllers/products.js";
 import { productValid } from "../validations/productValid.js";
 import { checkRequestBody } from "../middlewares/checkRequestBody.js";
 import { checkPermission } from "../middlewares/checkPermission.js";
-
 
 const routerProduct = Router();
 routerProduct.get("/", getAllProduct);
 routerProduct.get("/:id", getDetailProductPopulate);
 routerProduct.get("/not-populate/:id", getDetailProduct);
-routerProduct.delete("/:id", checkPermission('delete_product'), deleteProduct);
-routerProduct.delete("/restore/:id", checkPermission('restore_product'), restoreProduct);
-routerProduct.use(checkRequestBody(productValid))
-routerProduct.post("/", checkPermission('create_product'), createProduct);
-routerProduct.put("/:id", checkPermission('update_product'), updateProduct);
+routerProduct.delete("/:id", checkPermission("delete_product"), deleteProduct);
+routerProduct.delete(
+  "/restore/:id",
+  checkPermission("restore_product"),
+  restoreProduct
+);
+routerProduct.use(checkRequestBody(productValid));
+routerProduct.post("/", createProduct);
+routerProduct.put("/:id", checkPermission("update_product"), updateProduct);
 
 export default routerProduct;
