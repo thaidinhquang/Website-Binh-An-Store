2bb3c58d5c76c89b9bbb8b60df68b6d4d0047de7
lvcuong25 <vietcuong2532k3@gmail.com>
2024-08-21 15:25:59 +0700

    Update ProductView.jsx

--- FE/src/components/SingleProduct/ProductView.jsx
+++ FE/src/components/SingleProduct/ProductView.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import {
   useTanstackMutation,
@@ -10,76 +10,114 @@ import { useParams } from "react-router-dom";
 import { AuthContext } from "../Auth/core/Auth";
 import ThinLove from "../icons/ThinLove";
 
-
-
-const productFake={
-  "name": "Smartphone XYZ",
-  "image": "https://example.com/images/smartphone-xyz.jpg",
-  "status": 1,
-  "gallery": [
-      "https://example.com/images/smartphone-xyz-1.jpg",
-      "https://example.com/images/smartphone-xyz-2.jpg",
-      "https://example.com/images/smartphone-xyz-3.jpg"
-  ],
-  "parameter": "6.5 inch display, 128GB storage",
-  "description": "A high-end smartphone with a 6.5-inch display, advanced camera system, and 128GB of storage.",
-  "discount": 10,
-  "featured": true,
-  "tags": [
-      "smartphone",
-      "electronics",
-      "mobile"
-  ],
-  "slug": "smartphone-xyz",
-  "attributes": [
+const ProductView = ({ className }) => {
+  const demoProduct = {
+    name: "Smartphone XYZ",
+    image:
+      "https://cdn.tgdd.vn/Products/Images/7077/306530/befit-watch-fit-den-fix-1.jpg",
+    status: 1,
+    gallery: [
+      "https://cdn.tgdd.vn/Products/Images/7077/306530/befit-watch-fit-vang-1.jpg",
+      "https://cdn.tgdd.vn/Products/Images/7077/306530/befit-watch-fit-hong-2-2.jpg",
+      "https://cdn.tgdd.vn/Products/Images/7077/306530/befit-watch-fit-xanh-la-1-3.jpg",
+    ],
+    parameter: "6.5 inch display, 128GB storage",
+    description:
+      "A high-end smartphone with a 6.5-inch display, advanced camera system, and 128GB of storage.",
+    discount: 10,
+    featured: true,
+    tags: ["smartphone", "electronics", "mobile"],
+    slug: "smartphone-xyz",
+    attributes: [
       {
-          "key": "Color",
-          "value": "Black"
+        key: "Display",
+        value: "5 inch",
       },
       {
-          "key": "Storage",
-          "value": "128GB"
-      }
-  ],
-  "active": true,
-  "category": "64dfe8b2c9b5a53c12345679",
-  "brand": "64dfe8b2c9b5a53c12345670",
-  "productItems": [
+        key: "Camera",
+        value: "16mp",
+      },
       {
-          "productId": "64dfe8b2c9b5a53c12345678",
-          "name": "Smartphone XYZ",
-          "price": 699.99,
-          "stock": 150,
-          "reviews": [
-              "64dfe8b2c9b5a53c87654321",
-              "64dfe8b2c9b5a53c87654322"
-          ],
-          "rating": 4.5,
-          "variants": [
-              {
-                  "key": "Color",
-                  "value": [
-                      "Black",
-                      "White",
-                      "Blue"
-                  ]
-              },
-              {
-                  "key": "Storage",
-                  "value": [
-                      "64GB",
-                      "128GB",
-                      "256GB"
-                  ]
-              }
-          ]
-      }
-  ],
-  "createdAt": "2024-08-20T10:00:00Z",
-  "updatedAt": "2024-08-20T12:00:00Z"
-}
-const ProductView = ({ className }) => {
+        key: "CPU",
+        value: "16mp",
+      },  {
+        key: "Độ phân giải",
+        value: "200 x 320 Pixels",
+      },  {
+        key: "Chất liệu mặt",
+        value: "Mặt kính nhựa",
+      },  {
+        key: "Chất liệu khung viền",
+        value: "Hợp kim nhôm",
+      },  {
+        key: "Tính năng cho sức khỏe:",
+        value: "Đếm số bước chânĐo nồng độ oxy (SpO2)Đo nhịp timTính quãng đường chạyTính lượng calories tiêu thụTheo dõi giấc ngủBài tập thở",
+      },
+    ],
+    active: true,
+    category: "64dfe8b2c9b5a53c12345679",
+    brand: "64dfe8b2c9b5a53c12345670",
+    productItems: [
+      {
+        productId: "64dfe8b2c9b5a53c12345671",
+        name: "Smartphone XYZ",
+        price: 699.99,
+        stock: 120,
+        reviews: ["64dfe8b2c9b5a53c87654321", "64dfe8b2c9b5a53c87654322"],
+        rating: 4.5,
+        variants: [
+          {
+            key: "Color",
+            value: "Red",
+          },
+          {
+            key: "Storage",
+            value: "128GB",
+          },
+        ],
+      },
+      {
+        productId: "64dfe8b2c9b5a53c12345638",
+        name: "Smartphone XYZ",
+        price: 799.99,
+        stock: 140,
+        reviews: ["64dfe8b2c9b5a53c87654321", "64dfe8b2c9b5a53c87654322"],
+        rating: 5,
+        variants: [
+          {
+            key: "Color",
+            value: "Green",
+          },
+          {
+            key: "Storage",
+            value: "128GB",
+          },
+        ],
+      },
+      {
+        productId: "64dfe8b2c9b5a53c12345618",
+        name: "Smartphone XYZ",
+        price: 899.99,
+        stock: 150,
+        reviews: ["64dfe8b2c9b5a53c87654321", "64dfe8b2c9b5a53c87654322"],
+        rating: 4.6,
+        variants: [
+          {
+            key: "Color",
+            value: "Black",
+          },
+          {
+            key: "Storage",
+            value: "526GB",
+          },
+        ],
+      },
+    ],
+    createdAt: "2024-08-20T10:00:00Z",
+    updatedAt: "2024-08-20T12:00:00Z",
+  };
   const { currentUser } = useContext(AuthContext);
+  const [variants, setVariant] = useState(null);
   const { data: wishlistProducts } = useTanstackQuery("wishlist/products");
   const { mutate: addToWishlist } = useTanstackMutation({
     path: `wishlist/add`,
@@ -109,11 +147,7 @@ const ProductView = ({ className }) => {
 
   console.log(product);
 
-
-
   const [quantity, setQuantity] = useState(1);
-
-
 
   const { mutate } = useTanstackMutation({
     path: `cart/add-item`,
@@ -131,13 +165,9 @@ const ProductView = ({ className }) => {
 
   const handleAddToCart = (event) => {
     event.preventDefault();
-    // const attributesId = Object.keys(selectedAttributes).map((attr) => attr);
-    // const valuesId = Object.values(selectedAttributes).map((attr) => attr._id);
-    mutate({ productId: product._id, quantity,  });
+
+    mutate({ productId: product._id, quantity });
   };
-
-
-
 
   const getStatus = (createdAt) => {
     const creationDate = new Date(createdAt);
@@ -146,7 +176,6 @@ const ProductView = ({ className }) => {
     return now - creationDate <= twoDays ? "Mới" : "";
   };
 
-
   const checkProductInWishlist = (product) => {
     return (
       wishlistProducts?.findIndex((item) => item.productId === product._id) !==
@@ -154,7 +183,14 @@ const ProductView = ({ className }) => {
     );
   };
 
+  useEffect(() => {
+    setVariant(() => demoProduct.productItems[0]);
+  }, []);
+  const handleSelectVariant = (variant) => {
+    setVariant(variant);
+  };
 
+  const [selectedImage, setSelectedImage] = useState(demoProduct.image); // State to hold the selected image
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -169,22 +205,34 @@ const ProductView = ({ className }) => {
       >
         <div
           data-aos="fade-right"
-          className="lg:w-1/2 xl:mr-[70px] lg:mr-[50px]"
+          className="lg:w-1/2 xl:mr-[70px] lg:mr-[50px] flex flex-col" // Added flex and flex-col for vertical layout
         >
-          <div className="w-full">
-            <div className="w-full h-[600px] border border-qgray-border flex justify-center items-center overflow-hidden relative mb-3">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="object-contain w-full"
-              />
+          <div className="w-full h-[600px] border border-qgray-border flex justify-center items-center overflow-hidden relative mb-3">
+            <img
+              src={selectedImage} // Use selected image
+              alt={demoProduct.name}
+              className="object-contain w-full"
+            />
 
-              {getStatus(product.createdAt) && (
-                <div className="w-[80px] h-[80px] rounded-full bg-red-500 text-qblack flex justify-center items-center text-xl font-medium absolute left-[30px] top-[30px]">
-                  {getStatus(product.createdAt)}
-                </div>
-              )}
-            </div>
+            {getStatus(demoProduct.createdAt) && (
+              <div className="w-[80px] h-[80px] rounded-full bg-red-500 text-qblack flex justify-center items-center text-xl font-medium absolute left-[30px] top-[30px]">
+                {getStatus(demoProduct.createdAt)}
+              </div>
+            )}
+          </div>
+
+          <div className="flex overflow-x-auto space-x-2 mb-3">
+            {" "}
+        
+            {demoProduct.gallery.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Gallery image ${index + 1}`}
+                className="w-1/4 h-auto cursor-pointer border" 
+                onClick={() => setSelectedImage(image)} 
+              />
+            ))}
           </div>
         </div>
 
@@ -194,43 +242,46 @@ const ProductView = ({ className }) => {
               data-aos="fade-up"
               className="text-xl font-medium text-qblack mb-4"
             >
-              {product.name}{" "}{productFake?.productItems.map((rating, index) => (rating.rating ? <span className="text-sm text-gray-500" key={index}>({rating.rating})</span> : null ))}
-              
+              {variants?.name}{" "}
+              {variants?.rating ? (
+                <span className="text-sm text-gray-500">
+                  ({variants?.rating})
+                </span>
+              ) : null}
             </p>
 
             <div
               data-aos="fade-up"
               className="flex space-x-2 items-center mb-7"
             >
-              <span className="text-sm font-500 text-qgray line-through mt-2">
-                {formatPrice(product?.priceOld)}
-              </span>
-
               <span className="text-2xl font-500 text-qred">
-                {formatPrice()}
+                {formatPrice(variants?.price)}
               </span>
             </div>
+            <div className="my-2">
+              <span>Số Lượng: {variants?.stock}</span>
+            </div>
 
-           
-                 <div className="flex flex-wrap mb-4">
-              <div className="mb-4 flex flex-wrap">
-                {productFake?.productItems.map((attribute) =>
-                  attribute.variants.map((varriantitem, i) => (
-                <div key={i} className="flex items-center"> {/* Thay đổi để hiện trên 1 dòng */}
-                      {/*<span>{varriantitem.key}</span>*/}
-                      {varriantitem.value.map((item, index) => (
-                        <div key={index} className="mb-4 mr-4">
-                          <p
-                            className={`btn cursor-pointer  hover:bg-gray-300`}
-                          >
-                            {item}
-                          </p>
-                        </div>
-                      ))}
-                    </div>
-                  ))
-                )}
-              </div>
+            <div className="flex gap-2 mb-4">
+              {demoProduct.productItems.map((productItem, i) => {
+                return (
+                  <div
+                    onClick={() => handleSelectVariant(productItem)}
+                    key={i}
+                    className={`border-2 flex gap-2 h-[50px]  items-center ${
+                      variants?.productId === productItem.productId
+                        ?"border-cyan-500"
+                        :  "border-black" 
+                    } p-2 cursor-pointer`}
+                  >
+                    {productItem.variants.map((item, index) => (
+                      <p key={index} className="text-black">
+                        {item.value}
+                      </p>
+                    ))}
+                  </div>
+                );
+              })}
             </div>
 
             <div
@@ -301,13 +352,39 @@ const ProductView = ({ className }) => {
               <p className="text-[13px] text-qgray leading-7">
                 <span className="text-qblack">Danh mục: </span>
 
-                {productFake?.category || "Không có danh mục"}
+                {demoProduct?.category || "Không có danh mục"}
               </p>
 
               <p className="text-[13px] text-qgray leading-7">
-                <span className="text-qblack">Nhãn hàng:</span> {productFake?.brand || "không có nhãn hàng"}
+                <span className="text-qblack">Nhãn hàng:</span>{" "}
+                {demoProduct?.brand || "không có nhãn hàng"}
               </p>
             </div>
+
+<div>
+  <table className="w-full">
+    <tbody>
+      {demoProduct?.attributes.map((attribute, index) => (
+        <tr
+          key={index}
+          className={` ${
+            index % 2 === 0
+              ? 'bg-gray-100' 
+              : 'bg-white' 
+          }`}
+        >
+          <td className="px-4 py-2 font-bold text-gray-800">{attribute.key}</td>
+          <td className="px-4 py-2 text-gray-600">{attribute.value}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
+
+
+
+
           </div>
         </div>
       </div>
